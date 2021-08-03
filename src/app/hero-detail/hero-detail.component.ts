81be8272865dbe1efef6221df090c213b93d64ac
nico1311 <71448116+nico1311@users.noreply.github.com>
2021-08-03 20:08:32 -0300

    Implemented hero detail as a separate view, finished routing

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -8,11 +11,26 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {
 
-  ngOnInit(): void {
   }
 
-  @Input() hero?: Hero;
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
